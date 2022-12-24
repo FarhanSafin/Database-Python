a99ec3be611b9df99c54160f4ebd5101837cfc5f
Farhan <farhansafin@gmail.com>
2022-12-24 20:43:26 +0600

    2 file added and tested python interpreter

---
+++ reading_records.py
@@ -0,0 +1,10 @@
+# Testing
+class Name:
+    def __init__(self, names, money):
+        self.name = names
+        self.money = money
+
+
+Farhan = Name("Farhan", "45")
+print(Farhan.name)
+print(Farhan.money)
---
+++ storing_records.py
@@ -0,0 +1,10 @@
+# Testing
+class Name:
+    def __init__(self, names, fav_col):
+        self.name = names
+        self.fav_col = fav_col
+
+
+Farhan = Name("Farhan", "Blue")
+print(Farhan.name)
+print(Farhan.fav_col)
