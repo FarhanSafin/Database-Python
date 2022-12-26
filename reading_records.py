f2b60ca25d411b417cec7d7cb4dd9ff3a1ce4017
Farhan <farhansafin@gmail.com>
2022-12-26 21:53:34 +0600

    Data retrieved from data file. Shown on display

--- reading_records.py
+++ reading_records.py
@@ -3,11 +3,13 @@ import Libraries.binary_file_read as read
 import Libraries.binary_file_append as append
 import Libraries.record_to_string_convert as convert
 from datetime import date, datetime
+import re
+from prettytable import PrettyTable
 
 file = read.openFile("Data")
 read.jumpToReadingOffset(0, file)
 total = read.readBytes(9999999999, file)
 string = total.decode()
 
-for string in string.split("******"):
-    print(string)
+for line in string.split("******"):
+    print(line)
